94411f6215d6adb6860a06a1b2494880ec168ed0
Muhammad Popoola <popoolama@yahoo.com>
2023-06-06 11:36:09 +0100

    features section

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,9 @@ import Navbar from "./Navbar"
 import Hero from "./Hero"
 import Button from "./Button"
 import Clients from "./Clients"
+import Features from "./Features"
 
 
 export {
-    Navbar, Hero, Button, Clients,
+    Navbar, Hero, Button, Clients, Features,
 }
